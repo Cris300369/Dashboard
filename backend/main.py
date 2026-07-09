671fdb89fd1537d5f747ddb14a4a5edbba7300df
GustosSelectivos <gojhangames@gmail.com>
2026-07-09 09:28:58 -0400

    ultimos ajustes

--- backend/main.py
+++ backend/main.py
@@ -32,6 +32,6 @@ async def root():
 app.include_router(analytics.router)
 
 if __name__ == "__main__":
-    host = os.getenv("HOST", "0.0.0.0")
+    host = os.getenv("HOST", "127.0.0.1")
     port = int(os.getenv("PORT", "8000"))
     uvicorn.run("main:app", host=host, port=port, reload=True)
